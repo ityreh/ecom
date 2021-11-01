926855796f939c676012e46c72101c49984f94a6
Yannick Rehberger <yr@ityreh.de>
2021-11-01 21:13:07 +0100

    feat(admin): management for fabrics, patterns and products

--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,34 @@
 from django.contrib import admin
 
-from .models import Product
+from .models import (Fabric, FabricImage, Pattern, PatternImage, Product,
+                     ProductImage)
 
-admin.site.register(Product)
+
+class FabricImageInline(admin.StackedInline):
+    model = FabricImage
+    extra = 1
+
+
+class PatternImageInline(admin.StackedInline):
+    model = PatternImage
+    extra = 1
+
+
+class ProductImageInline(admin.StackedInline):
+    model = ProductImage
+    extra = 1
+
+
+@admin.register(Pattern)
+class PatternInline(admin.ModelAdmin):
+    inlines = [PatternImageInline]
+
+
+@admin.register(Fabric)
+class FabricAdmin(admin.ModelAdmin):
+    inlines = [FabricImageInline]
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    inlines = [ProductImageInline]
