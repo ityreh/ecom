1e8d1d4f0d4dc60e474e09edfa17670309a98897
Yannick Rehberger <yr@ityreh.de>
2021-11-03 21:29:55 +0100

    feat(products): add more to model (has to be refactored)

--- products/models.py
+++ products/models.py
@@ -1,8 +1,49 @@
 import uuid
 
 from django.db import models
-from django.db.models.deletion import CASCADE
+from django.db.models.deletion import CASCADE, PROTECT
+from django.db.models.fields.related import ForeignKey
 from django.utils.timezone import now
+
+
+class ProductImage(models.Model):
+    label = models.CharField(max_length=200)
+    image = models.ImageField()
+
+    def __str__(self):
+        return self.label
+
+
+class FabricImage(models.Model):
+    label = models.CharField(max_length=200)
+    image = models.ImageField()
+
+    def __str__(self):
+        return self.label
+
+
+class PatternImage(models.Model):
+    label = models.CharField(max_length=200)
+    image = models.ImageField()
+
+    def __str__(self):
+        return self.label
+
+
+class Fabric(models.Model):
+    label = models.CharField(max_length=200)
+    images = ForeignKey(FabricImage, on_delete=CASCADE)
+
+    def __str__(self):
+        return self.label
+
+
+class Pattern(models.Model):
+    label = models.CharField(max_length=200)
+    images = ForeignKey(PatternImage, on_delete=CASCADE)
+
+    def __str__(self):
+        return self.label
 
 
 class Product(models.Model):
@@ -13,49 +54,9 @@ class Product(models.Model):
     size = models.CharField(max_length=10)
     price = models.FloatField()
     quantity = models.IntegerField(default=0)
-
-    def __str__(self):
-        return self.label
-
-
-class Fabric(models.Model):
-    label = models.CharField(max_length=200)
-    product = models.ManyToManyField(Product)
-
-    def __str__(self):
-        return self.label
-
-
-class Pattern(models.Model):
-    label = models.CharField(max_length=200)
-    product = models.OneToOneField(Product, on_delete=CASCADE)
-
-    def __str__(self):
-        return self.label
-
-
-class ProductImage(models.Model):
-    label = models.CharField(max_length=200)
-    image = models.ImageField()
-    product = models.ForeignKey(Product, on_delete=CASCADE)
-
-    def __str__(self):
-        return self.label
-
-
-class FabricImage(models.Model):
-    label = models.CharField(max_length=200)
-    image = models.ImageField()
-    fabric = models.ForeignKey(Fabric, on_delete=CASCADE)
-
-    def __str__(self):
-        return self.label
-
-
-class PatternImage(models.Model):
-    label = models.CharField(max_length=200)
-    image = models.ImageField()
-    pattern = models.ForeignKey(Pattern, on_delete=CASCADE)
+    pattern = models.ForeignKey(Pattern, on_delete=PROTECT)
+    fabrics = models.ManyToManyField(Fabric)
+    images = ForeignKey(ProductImage, on_delete=CASCADE)
 
     def __str__(self):
         return self.label
